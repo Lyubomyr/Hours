7b24ab6d697b56ab4707157940a399e94afb6529
Lyubomyr Fedorniak <fedorniakl@gmail.com>
2017-04-24 14:14:34 +0300

    Update deploy

---
+++ deploy/config/recipes/nginx.rb
@@ -0,0 +1,29 @@
+namespace :nginx do
+  desc "Setup all NGINX configuration"
+  task :setup do
+    on roles(:all) do
+      sudo "rm -f #{fetch(:nginx_app_conf_path)}/default"
+      template "nginx.conf.erb", fetch(:nginx_conf_path)
+      template "nginx_app.erb", "#{fetch(:nginx_app_conf_path)}/#{fetch(:application)}"
+    end
+  end
+
+  task :sync do
+    on roles(:all) do
+      sync fetch(:nginx_conf_path), "nginx", clear: true
+      sync fetch(:nginx_app_conf_path), "nginx", recursive: true
+    end
+  end
+
+  %w[start stop restart reload force-reload status configtest rotate upgrade].each do |command|
+    desc "#{command} nginx server"
+    task command do
+      on roles(:app), except: {no_release: true} do
+        exec = "/etc/init.d/nginx #{command}"
+        puts exec
+        sudo exec
+      end
+    end
+  end
+
+end
